ed6487164fb0edef00216aa0e087014753d1969a
Alan Compton <112966271+AlanCompton@users.noreply.github.com>
2023-01-26 14:33:44 -0600

    Delete adder.js

--- adder.js
+++
@@ -1,7 +0,0 @@
-// ASSIGNMENT : Variable Math
-
-let num1 = 23;
-let num2 = 20;
-let sum = num1 + num2;
-
-console.log(`My girlfriend is ${num2} and I am ${num1}. Together, our combined age is equal to ${sum}.`);
